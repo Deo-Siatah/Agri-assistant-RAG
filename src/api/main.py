a2b5bd1222e4ed4ac4913dae5abd03a7f92a2e4d
Deo-Siatah <deosiatah0@gmail.com>
2026-08-05 21:04:05 +0300

    add a cors origin

--- src/api/main.py
+++ src/api/main.py
@@ -55,7 +55,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://localhost:5174"],
+    allow_origins=["http://localhost:5173","http://localhost:5174","https://agri-assistant-rag.vercel.app"],
     allow_credentials=True,
     allow_methods=["GET", "POST"],
     allow_headers=["*"],
